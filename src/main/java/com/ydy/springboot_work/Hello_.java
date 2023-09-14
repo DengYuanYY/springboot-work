e9a64247c66cd65365202ee1b96dac09479106f2
Darren <Darren@gmail.com>
2023-09-14 13:52:15 +0800

    1.0

---
+++ src/main/java/com/ydy/springboot_work/Hello_.java
@@ -0,0 +1,12 @@
+package com.ydy.springboot_work;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello_ {
+    @RequestMapping("/hello")
+    public String test(){
+        return "1111111";
+    }
+}
---
+++ src/main/java/com/ydy/springboot_work/SpringBoot_Main.java
@@ -0,0 +1,11 @@
+package com.ydy.springboot_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot_Main {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot_Main.class,args);
+    }
+}
